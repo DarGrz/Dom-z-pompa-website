c34d1c2b43fb7e0b651059d5c0d7f9fe4d4ee5a0
DarGrz <102190066+DarGrz@users.noreply.github.com>
2022-06-24 13:37:56 +0200

    Created json text info

---
+++ app.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    fetch("website_text.json")
+    .then(response => response.json())
+    .then(data => dataReady(data))
+}
+
+function dataReady(data) {
+    console.log(data);
+    document.querySelector("#short-txt").innerHTML = data.section1;
+    document.querySelector(".zbinv").innerHTML = data.section2.p1;
+    document.querySelector(".zbinv").innerHTML = data.section2.p1;
+    document.querySelector(".celtext").innerHTML = data.section3.celtext;
+    
+    
+
+    
+}
